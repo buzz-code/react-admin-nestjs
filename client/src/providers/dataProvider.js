158381688181a913144935b7a63cdf159f18474d
Hadasa Schechter <72219643+buzz-code@users.noreply.github.com>
2022-11-13 21:38:40 +0200

    update client to take api url from env var

--- client/src/providers/dataProvider.js
+++ client/src/providers/dataProvider.js
@@ -2,8 +2,10 @@
 // import crudProvider from 'ra-data-nestjsx-crud'
 import crudProvider from '@fusionworks/ra-data-nest-crud';
 
+const dataUrl = process.env.API_URL || 'http://localhost:3000';
+
 // const dataProvider = jsonServerProvider('http://localhost:3000');
 // const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-const dataProvider = crudProvider('http://localhost:3000');
+const dataProvider = crudProvider(dataUrl);
 
 export default dataProvider;
