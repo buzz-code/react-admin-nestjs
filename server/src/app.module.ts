52720b951b0b68654548f728fc8e008c018dc974
buzz-code <hadasa.schechter@gmail.com>
2022-11-25 11:07:23 +0000

    update use module to be one file

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import TypeOrmModule from './typeorm.module';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './entity-modules/users.module';
 import { AttReportsModule } from './entity-modules/att_reports.module';
 import { GradesModule } from './entity-modules/grades.module';
 import { KlassesModule } from './entity-modules/klasses.module';
