538b9b63e8b2f09ed40bf3f0bd0034d2aade7da4
buzz-code <hadasa.schechter@gmail.com>
2022-12-04 16:42:28 +0000

    update react admin version & menu

--- client/src/components/common/Menu.jsx
+++ client/src/components/common/Menu.jsx
@@ -1,38 +1,17 @@
-import { createElement } from 'react';
-import { Menu, useResourceDefinitions, useGetResourceLabel, useCreatePath } from 'react-admin';
-import DefaultIcon from '@mui/icons-material/ViewList';
+import { Menu, useResourceDefinitions } from 'react-admin';
 import LabelIcon from '@mui/icons-material/Label';
 
-const CustomMenu = ({ hasDashboard, ...props }) => {
+const CustomMenu = ({ hasDashboard }) => {
     const resources = useResourceDefinitions();
-    const getResourceLabel = useGetResourceLabel();
-    const createPath = useCreatePath();
 
     return (
         <Menu>
             {hasDashboard && (
                 <Menu.DashboardItem key="default-dashboard-menu-item" />
             )}
-            {Object.keys(resources)
-                .filter(name => resources[name].hasList)
-                .map(name => (
-                    <Menu.Item
-                        key={name}
-                        to={createPath({
-                            resource: name,
-                            type: 'list',
-                        })}
-                        state={{ _scrollToTop: true }}
-                        primaryText={getResourceLabel(name, 2)}
-                        leftIcon={
-                            resources[name].icon ? (
-                                createElement(resources[name].icon)
-                            ) : (
-                                <DefaultIcon />
-                            )
-                        }
-                    />
-                ))}
+            {Object.keys(resources).map(name => (
+                <Menu.ResourceItem key={name} name={name} />
+            ))}
             <Menu.Item to="/yemot-simulator" primaryText="סימולטור" leftIcon={<LabelIcon />} />
         </Menu>
     );
