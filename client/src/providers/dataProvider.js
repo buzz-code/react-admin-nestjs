07dfdfd9a74baa803e983af32f4f55f3e1a92412
buzz-code <hadasa.schechter@gmail.com>
2022-12-05 06:18:15 +0000

    update data provider

--- client/src/providers/dataProvider.js
+++ client/src/providers/dataProvider.js
@@ -1,5 +1,7 @@
 import crudProvider from 'ra-data-nestjsx-crud';
 import { fetchUtils } from 'react-admin';
+import { RequestQueryBuilder } from '@nestjsx/crud-request';
+import { stringify } from 'query-string';
 
 import { apiUrl } from './constantsProvider';
 
@@ -15,18 +17,53 @@ const fetchJson = (url, options = {}) => {
 
 const dataProvider = crudProvider(apiUrl, fetchJson);
 
+export default dataProvider;
+
+
+const composeFilter = (paramsFilter) => {
+    const flatFilter = fetchUtils.flattenObject(paramsFilter);
+    return Object.keys(flatFilter).map((key) => {
+        const splitKey = key.split(/\|\||:/)
+        const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/gi;
+
+        let field = splitKey[0];
+        let ops = splitKey[1];
+        if (!ops) {
+            if (typeof flatFilter[key] === 'boolean' || typeof flatFilter[key] === 'number' || (typeof flatFilter[key] === 'string' && (flatFilter[key].match(/^\d+$/)) || flatFilter[key].match(uuidRegex))) {
+                ops = CondOperator.EQUALS;
+            } else {
+                ops = CondOperator.CONTAINS_LOW;
+            }
+        }
+
+        if (field.startsWith('_') && field.includes('.')) {
+            field = field.split(/\.(.+)/)[1];
+        }
+        return { field, operator: ops, value: flatFilter[key] };
+    });
+};
+
+const composeQueryParams = (queryParams = {}) => {
+    return stringify(fetchUtils.flattenObject(queryParams), { skipNull: true });
+}
+
+const mergeEncodedQueries = (...encodedQueries) => encodedQueries.map((query) => query).join('&')
+
+
 dataProvider.getCount = (resource, params) => {
-    // const _a = params.filter || {}, { q: queryParams, $OR: orFilter } = _a, filter = __rest(_a, ["q", "$OR"]);
-    // const encodedQueryParams = composeQueryParams(queryParams);
-    // const encodedQueryFilter = RequestQueryBuilder.create({
-    //     filter: composeFilter(filter),
-    //     or: composeFilter(orFilter || {})
-    // })
-    //     .query();
-    // const query = mergeEncodedQueries(encodedQueryParams, encodedQueryFilter);
-    const query = '';
+    const { q: queryParams, $OR: orFilter, ...filter } = params.filter || {};
+
+    const encodedQueryParams = composeQueryParams(queryParams)
+    const encodedQueryFilter = RequestQueryBuilder.create({
+        filter: composeFilter(filter),
+        or: composeFilter(orFilter || {})
+    })
+        .query();
+
+    const query = mergeEncodedQueries(encodedQueryParams, encodedQueryFilter);
+
     const url = `${apiUrl}/${resource}/get-count?${query}`;
-    return fetchJson(url).then(response => response.json).then(({ count }) => count);
+    return fetchJson(url).then(({ json }) => json.count);
 };
 
 dataProvider.simulateYemotCall = async (body) => fetchJson(
@@ -35,5 +72,3 @@ dataProvider.simulateYemotCall = async (body) => fetchJson(
         method: 'POST',
         body: JSON.stringify(body)
     })
-
-export default dataProvider;
