1cc284e77334e8f4081c57ff601fed8109710a63
buzz-code <hadasa.schechter@gmail.com>
2022-12-05 16:06:40 +0000

    add migration boilerplate

---
+++ server/src/config/database.config.ts
@@ -0,0 +1,17 @@
+import { DataSourceOptions } from "typeorm";
+
+export const databaseConfig: DataSourceOptions = {
+    type: 'mysql',
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT),
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    // entityPrefix: 'miryam_peperman',
+    // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    // synchronize: true,
+    logging: "all",
+    // migrationsRun: true,
+    // migrationsTableName: 'migration',
+    // migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+};
--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -1,20 +1,7 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { DataSourceOptions } from "typeorm";
-
-export const typeOrmConfig: DataSourceOptions = {
-    type: 'mysql',
-    host: process.env.MYSQL_HOST,
-    port: Number(process.env.MYSQL_PORT),
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    // synchronize: true,
-    logging: "all",
-};
-
-export default typeOrmConfig;
+import { databaseConfig } from "./database.config";
 
 export const typeOrmModuleConfig: TypeOrmModuleOptions = {
-    ...typeOrmConfig,
+    ...databaseConfig,
     autoLoadEntities: true,
 }
