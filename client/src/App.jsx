199960577163ceb2a179f3c4b03ac131c8a6053f
buzz-code <hadasa.schechter@gmail.com>
2022-12-08 22:04:46 +0000

    temporary hide comps

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import { getI18nProvider } from "@buzz-code/crud-nestjs-react-admin/client/provi
 import authProvider from "@buzz-code/crud-nestjs-react-admin/client/providers/authProvider";
 import theme from "@buzz-code/crud-nestjs-react-admin/client/providers/themeProvider";
 
-import { Dashboard, Layout } from './GeneralLayout';
+// import { Dashboard, Layout } from './GeneralLayout';
 
 import { AttReportEdit, AttReportCreate, AttReportList } from "./entities/att-report";
 import { KlassEdit, KlassCreate, KlassList } from "./entities/klass";
@@ -21,13 +21,13 @@ import { TextEdit, TextCreate, TextList } from "./entities/text";
 import { UserEdit, UserCreate, UserList } from "./entities/user";
 import { isAdmin } from "@buzz-code/crud-nestjs-react-admin/client/components/AdminRestricted";
 import { CommonRepresentation } from "@buzz-code/crud-nestjs-react-admin/client/components/CommonRepresentation";
-import YemotSimulator from "@buzz-code/crud-nestjs-react-admin/client/components/YemotSimulator";
+// import YemotSimulator from "@buzz-code/crud-nestjs-react-admin/client/components/YemotSimulator";
 
 const i18nProvider = getI18nProvider(domainTranslations);
 
 const App = () => (
   <BrowserRouter>
-    <Admin dataProvider={dataProvider} i18nProvider={i18nProvider} authProvider={authProvider} theme={theme} dashboard={Dashboard} layout={Layout} requireAuth>
+    <Admin dataProvider={dataProvider} i18nProvider={i18nProvider} authProvider={authProvider} theme={theme} requireAuth>
       {permissions => (
         <>
           <Resource name="att_report" list={AttReportList} edit={AttReportEdit} create={AttReportCreate} />
@@ -46,7 +46,7 @@ const App = () => (
           </>}
 
           <CustomRoutes>
-            <Route path="/yemot-simulator" element={<YemotSimulator />} />
+            {/* <Route path="/yemot-simulator" element={<YemotSimulator />} /> */}
           </CustomRoutes>
         </>
       )}
