088a94ec4d552df9886557faa62b59c562f9d034
buzz-code <hadasa.schechter@gmail.com>
2022-12-14 16:21:29 +0000

    delete old common files

--- server/src/common/config/typeorm.config.ts
+++
@@ -1,8 +0,0 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { databaseConfig } from "./database.config";
-
-export const typeOrmModuleConfig: TypeOrmModuleOptions = {
-    ...databaseConfig,
-    entities: undefined,
-    autoLoadEntities: true,
-}
