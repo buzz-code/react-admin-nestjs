7ff84dc80d45d1b53f14200c87db6c6ccef47200
buzz-code <hadasa.schechter@gmail.com>
2022-12-14 20:30:11 +0000

    auth with cookie

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Request, Res, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthService } from '@shared/auth/auth.service';
 import { JwtAuthGuard } from '@shared/auth/jwt-auth.guard';
 import { LocalAuthGuard } from '@shared/auth/local-auth.guard';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -12,8 +13,17 @@ export class AppController {
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
+  @HttpCode(200)
+  async login(@Request() req, @Res() response: Response) {
+    const cookie = await this.authService.getCookieWithJwtToken(req.user);
+    response.setHeader('Set-Cookie', cookie);
+    return response.send({ success: true });
+  }
+
+  @Post('auth/logout')
+  async logOut(@Res() response: Response) {
+    response.setHeader('Set-Cookie', this.authService.getCookieForLogOut());
+    return response.sendStatus(200);
   }
 
   @UseGuards(JwtAuthGuard)
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,15 @@
 import '@shared/config/crud.config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+    origin: true,
+  });
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
