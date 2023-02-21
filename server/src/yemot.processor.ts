9f876280570785c382fdb619c07c14d66c50d5a3
buzz-code <hadasa.schechter@gmail.com>
2023-02-21 16:00:00 +0000

    update yemot flow

--- server/src/yemot.processor.ts
+++ server/src/yemot.processor.ts
@@ -2,28 +2,35 @@ import { YemotProcessor, YemotProcessorProvider, YEMOT_HANGUP_STEP, YEMOT_NOT_IM
 import { YemotCall, YemotParams } from "@shared/entities/YemotCall.entity";
 import yemotUtil from "@shared/utils/yemot/yemot.util";
 import { Teacher } from "./db/entities/Teacher.entity";
+import { Lesson } from "./db/entities/Lesson.entity";
 
 export const yemotProcessorProvider: YemotProcessorProvider = (dataSource) => new YemotProcessorImpl(dataSource);
 
 class YemotProcessorImpl extends YemotProcessor {
     steps = {
         initial: 'initial',
-        getTeacherId: 'got-teacher-id',
+        getLessonId: 'got-lesson-id',
         [YEMOT_HANGUP_STEP]: YEMOT_HANGUP_STEP,
         [YEMOT_NOT_IMPL_STEP]: YEMOT_NOT_IMPL_STEP,
     };
+    params = {
+        lessonId: 'lessonId'
+    }
 
     async processCall(activeCall: YemotCall, body: YemotParams): Promise<{ response: string; nextStep: string; }> {
         switch (activeCall.currentStep) {
             case this.steps.initial: {
                 try {
-                    const teacher = await this.dataSource.getRepository(Teacher).findOneByOrFail([{ phone: activeCall.phone }, { phone2: activeCall.phone }]);
+                    const teacher = await this.dataSource.getRepository(Teacher).findOneByOrFail([
+                        { phone: activeCall.phone },
+                        { phone2: activeCall.phone }
+                    ]);
                     return {
                         response: yemotUtil.send(
                             yemotUtil.id_list_message_v2(await this.getText('welcome')),
-                            yemotUtil.read_v2(await this.getText('type teacher id', teacher.name), 'teacherId', { min: 1, max: 9 })
+                            yemotUtil.read_v2(await this.getText('type lesson id', teacher.name), this.params.lessonId, { min: 1, max: 9 })
                         ),
-                        nextStep: this.steps.getTeacherId,
+                        nextStep: this.steps.getLessonId,
                     }
                 } catch (e) {
                     console.log(e)
@@ -35,11 +42,14 @@ class YemotProcessorImpl extends YemotProcessor {
                     }
                 }
             }
-            case this.steps.getTeacherId: {
-                // const teacher = await this.teacherRepo.findOneBy({id: body.teacherId});
+            case this.steps.getLessonId: {
+                const lesson = await this.dataSource.getRepository(Lesson).findOneByOrFail({
+                    userId: activeCall.userId,
+                    key: Number(body[this.params.lessonId])
+                });
                 return {
                     response: yemotUtil.send(
-                        yemotUtil.read_v2('great ' + body.teacherId, 'nothing')
+                        yemotUtil.read_v2(await this.getText('great ', body[this.params.lessonId], lesson.name), 'nothing')
                     ),
                     nextStep: this.steps[YEMOT_HANGUP_STEP],
                 }
