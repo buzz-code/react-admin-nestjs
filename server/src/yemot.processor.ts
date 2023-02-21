a703a520214855f863d2297ec4ea4a5720ad9f6f
buzz-code <hadasa.schechter@gmail.com>
2023-02-21 18:22:15 +0000

    update yemot processor

--- server/src/yemot.processor.ts
+++ server/src/yemot.processor.ts
@@ -9,59 +9,88 @@ export const yemotProcessorProvider: YemotProcessorProvider = (dataSource) => ne
 class YemotProcessorImpl extends YemotProcessor {
     steps = {
         initial: 'initial',
-        getLessonId: 'got-lesson-id',
+        gotLessonId: 'got-lesson-id',
         [YEMOT_HANGUP_STEP]: YEMOT_HANGUP_STEP,
         [YEMOT_NOT_IMPL_STEP]: YEMOT_NOT_IMPL_STEP,
     };
     params = {
-        lessonId: 'lessonId'
+        lessonId: 'lessonId',
+        lessonConfirm: 'lessonConfirm',
     }
 
     async processCall(activeCall: YemotCall, body: YemotParams): Promise<{ response: string; nextStep: string; }> {
         switch (activeCall.currentStep) {
             case this.steps.initial: {
-                try {
-                    const teacher = await this.dataSource.getRepository(Teacher).findOneByOrFail([
-                        { phone: activeCall.phone },
-                        { phone2: activeCall.phone }
-                    ]);
-                    return {
-                        response: yemotUtil.send(
-                            yemotUtil.id_list_message_v2(await this.getText('welcome')),
-                            yemotUtil.read_v2(await this.getText('type lesson id', teacher.name), this.params.lessonId, { min: 1, max: 9 })
-                        ),
-                        nextStep: this.steps.getLessonId,
-                    }
-                } catch (e) {
-                    console.log(e)
-                    return {
-                        response: yemotUtil.send(
-                            yemotUtil.id_list_message_v2(await this.getText('no teacher found')),
-                        ),
-                        nextStep: this.steps[YEMOT_HANGUP_STEP],
-                    }
+                const teacher = await this.helpers.getTeacherByPhone(activeCall);
+                if (!teacher) {
+                    return this.responses.teacherNotFound(activeCall);
                 }
+
+                activeCall.data.teacher = teacher;
+                return this.responses.getLesson(activeCall);
             }
-            case this.steps.getLessonId: {
-                const lesson = await this.dataSource.getRepository(Lesson).findOneByOrFail({
-                    userId: activeCall.userId,
-                    key: Number(body[this.params.lessonId])
-                });
-                return {
-                    response: yemotUtil.send(
-                        yemotUtil.read_v2(await this.getText('great ', body[this.params.lessonId], lesson.name), 'nothing')
-                    ),
-                    nextStep: this.steps[YEMOT_HANGUP_STEP],
+            case this.steps.gotLessonId: {
+                const lesson = await this.helpers.getLesson(activeCall, Number(body[this.params.lessonId]));
+                if (!lesson) {
+                    return this.responses.retryGetLesson(activeCall);
                 }
+
+                activeCall.data.lesson = lesson;
+                return this.responses.sendLessonName(activeCall);
             }
             default: {
-                return {
-                    response: yemotUtil.send(
-                        yemotUtil.hangup()
-                    ),
-                    nextStep: this.steps[YEMOT_NOT_IMPL_STEP],
-                }
+                return this.responses.hangup(activeCall);
             }
         }
+
+    }
+
+    helpers = {
+        getTeacherByPhone: (activeCall: YemotCall) => {
+            return this.dataSource.getRepository(Teacher).findOneBy([
+                { userId: activeCall.userId, phone: activeCall.phone },
+                { userId: activeCall.userId, phone2: activeCall.phone }
+            ]);
+        },
+        getLesson: (activeCall: YemotCall, lessonId: number) => {
+            return this.dataSource.getRepository(Lesson).findOneBy({
+                userId: activeCall.userId,
+                key: lessonId
+            });
+        }
+    }
+
+    responses = {
+        teacherNotFound: async (activeCall: YemotCall) => ({
+            response: yemotUtil.send(
+                yemotUtil.id_list_message_v2(await this.getText(activeCall.userId, 'phoneIsNotRecognizedInTheSystem')),
+            ),
+            nextStep: this.steps[YEMOT_HANGUP_STEP],
+        }),
+        getLesson: async (activeCall: YemotCall) => ({
+            response: yemotUtil.send(
+                yemotUtil.read_v2(await this.getText(activeCall.userId, 'typeLessonId', activeCall.data.teacher.name), this.params.lessonId, { max: 9, min: 1, block_asterisk: true })
+            ),
+            nextStep: this.steps.gotLessonId,
+        }),
+        retryGetLesson: async (activeCall: YemotCall) => ({
+            response: yemotUtil.send(
+                yemotUtil.id_list_message_v2(await this.getText(activeCall.userId, 'lessonIdNotFound')),
+                yemotUtil.read_v2(await this.getText(activeCall.userId, 'tryAgain'), this.params.lessonId, { max: 9, min: 1, block_asterisk: true })
+            ),
+            nextStep: this.steps.gotLessonId,
+        }),
+        sendLessonName: async (activeCall: YemotCall) => ({
+            response: yemotUtil.send(
+                yemotUtil.read_v2(await this.getText(activeCall.userId, 'confirmLesson', activeCall.data.lesson.name), this.params.lessonConfirm, { max: 1, min: 1, block_asterisk: true })
+            ),
+            nextStep: this.steps[YEMOT_HANGUP_STEP],
+        }),
+        hangup: async (activeCall: YemotCall) => ({
+            response: yemotUtil.send(
+                yemotUtil.hangup()
+            ),
+            nextStep: this.steps[YEMOT_NOT_IMPL_STEP],
+        }),
     }
 }
