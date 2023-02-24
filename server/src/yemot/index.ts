8d56dbebaacd20cedb657960d0e0f831488b916a
buzz-code <hadasa.schechter@gmail.com>
2023-02-24 11:38:23 +0000

    add createBaseReportHandler step

--- server/src/yemot/index.ts
+++ server/src/yemot/index.ts
@@ -14,22 +14,37 @@ const lessonChain = getResourceConfirmationChain('lesson', getLessonFromLessonId
 function getKlassFromKlassId(req: YemotRequest) {
     return req.dataSource.getLessonFromLessonId(req.params.klass.id);
 };
-const klassFromLessonHandler = new Handler(async (req, res, callback) => {
-    if (!req.params.teacher?.data) {
+const klassFromLessonHandler = new Handler(async (req, res, next) => {
+    if (req.params.teacher?.data === undefined) {
         if (req.params.lesson.data.klasses && !req.params.lesson.data.klasses.includes(',')) {
             req.params.teacher = {
                 id: req.params.lesson.data.klasses
             };
         }
     }
-    return callback();
+    return next();
 });
 const klassChain = getResourceConfirmationChain('klass', getKlassFromKlassId);
+
+
+const createBaseReportHandler = new Handler(async (req, res, next) => {
+    if (req.params.baseReport === undefined) {
+        req.params.baseReport = {
+            userId: req.params.userId,
+            teacherReferenceId: req.params.teacher.id,
+            klassReferenceId: req.params.klass.data.id,
+            lessonReferenceId: req.params.lesson.data.id,
+            reportDate: new Date(),
+        };
+    }
+    next();
+});
 
 
 export default new Chain([
     teacherChain,
     lessonChain,
     klassFromLessonHandler,
-    klassChain
+    klassChain,
+    createBaseReportHandler,
 ]);
