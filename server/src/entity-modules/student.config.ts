1a1d2bba19b75b101604d0ba2d36f2d4e76b21cd
buzz-code <hadasa.schechter@gmail.com>
2023-03-21 12:33:37 +0000

    add pivot logic for student entity

--- server/src/entity-modules/student.config.ts
+++ server/src/entity-modules/student.config.ts
@@ -1,6 +1,12 @@
-import { BaseEntityModuleOptions } from "@shared/base-entity/interface";
+import { CrudRequestOptions, GetManyDefaultResponse, Override } from "@dataui/crud";
+import { InjectDataSource } from "@nestjs/typeorm";
+import { BaseEntityService } from "@shared/base-entity/base-entity.service";
+import { BaseEntityModuleOptions, Entity } from "@shared/base-entity/interface";
 import { IHeader } from "@shared/utils/exporter/types";
 import { Student } from "src/db/entities/Student.entity";
+import { DataSource, In, SelectQueryBuilder } from "typeorm";
+import { ParsedRequestParams } from "@dataui/crud-request";
+import { AttReport } from "src/db/entities/AttReport.entity";
 
 function getConfig(): BaseEntityModuleOptions {
     return {
@@ -13,6 +19,50 @@ function getConfig(): BaseEntityModuleOptions {
                 ];
             }
         },
+        service: StudentService,
+    }
+}
+
+class StudentService<T extends Entity | Student> extends BaseEntityService<T> {
+    @InjectDataSource()
+    private dataSource: DataSource;
+
+    @Override()
+    protected async doGetMany(builder: SelectQueryBuilder<T>, query: ParsedRequestParams, options: CrudRequestOptions): Promise<T[] | GetManyDefaultResponse<T>> {
+        const res = await super.doGetMany(builder, query, options);
+        const list = Array.isArray(res) ? res : res.data;
+        if (list.length > 0) {
+            await this.populatePivotData(list as Student[]);
+        }
+        return res;
+    }
+
+    private async populatePivotData(data: Student[]) {
+        const studentIds = data.map(item => item.id);
+        const studentMap = data.reduce((a, b) => ({ ...a, [b.id]: b }), {});
+
+        const pivotData = await this.dataSource
+            .getRepository(AttReport)
+            .find({
+                where: {
+                    userId: data[0].userId,
+                    studentReferenceId: In(studentIds),
+                },
+                relations: {
+                    lesson: true,
+                    teacher: true,
+                }
+            })
+
+        pivotData.forEach(item => {
+            const key = `${item.lessonReferenceId}_${item.teacherReferenceId}`;
+            if (studentMap[item.studentReferenceId][key] === undefined) {
+                studentMap[item.studentReferenceId][key] = 0;
+                studentMap[item.studentReferenceId][`${key}_title`] = `${item.lesson?.name} המו\' ${item.teacher?.name}`;
+            }
+            studentMap[item.studentReferenceId][key] += item.absCount;
+            studentMap[item.studentReferenceId].total = (studentMap[item.studentReferenceId].total || 0) + item.absCount;
+        })
     }
 }
 
