58794099b2f12d59ea8218cd0ca54d378d3a95c6
buzz-code <hadasa.schechter@gmail.com>
2023-03-22 08:58:39 +0000

    add migration to add mail_address table

---
+++ server/src/migrations/1679433140359-addMailAddressTable.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class addMailAddressTable1679433140359 implements MigrationInterface {
+    name = 'addMailAddressTable1679433140359'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE \`mail_address\` (
+                \`id\` int NOT NULL AUTO_INCREMENT,
+                \`userId\` int NOT NULL,
+                \`alias\` varchar(255) NOT NULL,
+                \`entity\` varchar(255) NOT NULL,
+                \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+                \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+                UNIQUE INDEX \`IDX_10d2242b0e45f6add0b4269cbf\` (\`userId\`, \`entity\`),
+                PRIMARY KEY (\`id\`)
+            ) ENGINE = InnoDB
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP INDEX \`IDX_10d2242b0e45f6add0b4269cbf\` ON \`mail_address\`
+        `);
+        await queryRunner.query(`
+            DROP TABLE \`mail_address\`
+        `);
+    }
+
+}
