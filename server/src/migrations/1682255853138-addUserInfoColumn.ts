b6ac891170046c317f23602dda92f962e6dee3fb
buzz-code <hadasa.schechter@gmail.com>
2023-04-23 17:57:32 +0000

    add userInfo migration

---
+++ server/src/migrations/1682255853138-addUserInfoColumn.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class addUserInfoColumn1682255853138 implements MigrationInterface {
+    name = 'addUserInfoColumn1682255853138'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            ALTER TABLE \`users\`
+            ADD \`userInfo\` text NULL
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            ALTER TABLE \`users\` DROP COLUMN \`userInfo\`
+        `);
+    }
+
+}
