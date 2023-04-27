e35ff139526d791986ee24293e914abb7bc076d6
buzz-code <hadasa.schechter@gmail.com>
2023-04-27 08:34:09 +0000

    update cors options

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     credentials: true,
-    origin: true,
+    origin: new RegExp('http(s?)://' + process.env.DOMAIN_NAME),
   });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
