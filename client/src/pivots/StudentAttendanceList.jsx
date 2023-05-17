9fe2e1fcc4b5d5a307829984e3921e9e3ffbeb0e
buzz-code <hadasa.schechter@gmail.com>
2023-05-17 13:08:31 +0000

    add filters to student attendance list

--- client/src/pivots/StudentAttendanceList.jsx
+++ client/src/pivots/StudentAttendanceList.jsx
@@ -1,11 +1,19 @@
 import { CommonDatagrid, CommonList } from "@shared/components/crudContainers/CommonList"
 import { useIsAdmin } from "@shared/utils/permissionsUtil";
-import { ReferenceField, TextField, useListContext } from "react-admin"
+import { ReferenceField, TextField, useListContext, ReferenceInput, TextInput } from "react-admin"
 
 export default (props) => {
+    const isAdmin = useIsAdmin();
+
+    const filters = [
+        isAdmin && <ReferenceInput source="userId" reference="user" />,
+        <TextInput source="tz:$cont" label="תז" />,
+        <TextInput source="name:$cont" alwaysOn />,
+    ].filter(item => item);
+
     return (
         <CommonList resource="student/pivot?extra.pivot=StudentAttendance"
-            importer={null} exporter={false} {...props}>
+            importer={null} exporter={false} filters={filters} {...props}>
             <Datagrid />
         </CommonList>
     );
@@ -21,7 +29,7 @@ const Datagrid = ({ ...props }) => {
         <TextField source="tz" />,
         <TextField source="name" />,
         ...(data?.[0]?.headers?.map(item => (
-            <TextField key={item.value} source={item.value} label={item.label} />
+            <TextField key={item.value} source={item.value} label={item.label} sortable={false} />
         )) ?? []),
     ]
 
