e41e7b99cfafc9fe9d3d1b0b6db605e06b707392
buzz-code <hadasa.schechter@gmail.com>
2023-05-18 10:53:23 +0000

    fix student klasses report sort by student name

--- server/src/db/view-entities/StudentKlassReport.entity.ts
+++ server/src/db/view-entities/StudentKlassReport.entity.ts
@@ -4,6 +4,7 @@ import { Klass } from "src/db/entities/Klass.entity";
 import { StudentKlass } from "src/db/entities/StudentKlass.entity";
 import { User } from "@shared/entities/User.entity";
 import { KlassType, KlassTypeEnum } from "../entities/KlassType.entity";
+import { Student } from "../entities/Student.entity";
 
 @ViewEntity("student_klasses_report", {
   expression: (dataSource: DataSource) => dataSource
@@ -11,12 +12,14 @@ import { KlassType, KlassTypeEnum } from "../entities/KlassType.entity";
     .createQueryBuilder('student_klasses')
     .leftJoin(Klass, 'klasses', 'klasses.id = student_klasses.klassReferenceId OR (klasses.key = student_klasses.klass_id AND klasses.user_id = student_klasses.user_id)')
     .leftJoin(KlassType, 'klass_types', 'klass_types.id = klasses.klassTypeReferenceId OR (klass_types.id = klasses.klass_type_id AND klass_types.user_id = klasses.user_id)')
-    .groupBy('student_klasses.studentReferenceId')
+    .leftJoin(Student, 'students', 'students.id = student_klasses.studentReferenceId OR (students.tz = student_klasses.student_tz AND students.user_id = student_klasses.user_id)')
+    .groupBy('students.id')
     .addGroupBy('student_klasses.student_tz')
     .addGroupBy('student_klasses.user_id')
     .addGroupBy('student_klasses.year')
-    .select('COALESCE(studentReferenceId, student_tz)', 'id')
+    .select('students.id', 'id')
     .addSelect('student_tz')
+    .addSelect('students.name', 'studentName')
     .addSelect('student_klasses.user_id', 'user_id')
     .addSelect('student_klasses.year', 'year')
     .addSelect(`GROUP_CONCAT(if(klass_types.klassTypeEnum = '${KlassTypeEnum.baseKlass}', klasses.name, null) SEPARATOR ', ')`, 'klasses_1')
@@ -26,7 +29,7 @@ import { KlassType, KlassTypeEnum } from "../entities/KlassType.entity";
 })
 export class StudentKlassReport implements IHasUserId {
   @Column()
-  id: string;
+  id: number;
 
   @Column("int", { name: "user_id" })
   userId: number;
@@ -36,6 +39,12 @@ export class StudentKlassReport implements IHasUserId {
 
   @Column("varchar", { name: "student_tz", length: 10, nullable: true })
   studentTz: string;
+
+  @Column({ name: "id" })
+  studentReferenceId: number;
+
+  @Column()
+  studentName: string;
 
   @Column({ name: "klasses_1" })
   klasses1: string;
