a4698b15df14ab7a0019ac186640d7f5ebf1580f
buzz-code <hadasa.schechter@gmail.com>
2023-05-29 19:36:35 +0000

    fix param name for lesson in yemot

--- server/src/yemot/yemot.chain.ts
+++ server/src/yemot/yemot.chain.ts
@@ -7,13 +7,13 @@ import { YemotRequest } from "@shared/utils/yemot/yemot.interface";
 
 
 function getLessonFromLessonId(req: YemotRequest) {
-    return req.getLessonFromLessonId(req.params.lesson.id);
+    return req.getLessonFromLessonId(req.params.lessonId);
 };
 const lessonChain = getResourceConfirmationChain('lesson', getLessonFromLessonId);
 
 
 function getKlassFromKlassId(req: YemotRequest) {
-    return req.getKlassByKlassId(req.params.klass.id);
+    return req.getKlassByKlassId(req.params.klassId);
 };
 const klassFromLessonHandler = new Handler(async (req, res, next) => {
     if (req.params.teacher?.data === undefined) {
