2c0d23e459b7f29367e46f314d6858e0ebbd05f7
buzz-code <hadasa.schechter@gmail.com>
2023-06-14 18:33:32 +0000

    add exporter for student-percent-report

--- server/src/entity-modules/student-percent-report.config.ts
+++ server/src/entity-modules/student-percent-report.config.ts
@@ -1,5 +1,11 @@
+import { CrudRequest } from "@dataui/crud";
 import { BaseEntityModuleOptions } from "@shared/base-entity/interface";
+import { IHeader } from "@shared/utils/exporter/types";
 import { StudentPercentReport } from "src/db/view-entities/StudentPercentReport.entity";
+
+const getPercentsFormatter = (value: string) =>
+    row =>
+        row[value] && !isNaN(row[value]) ? `${Number(row[value]) * 100}%` : null;
 
 function getConfig(): BaseEntityModuleOptions {
     return {
@@ -12,6 +18,29 @@ function getConfig(): BaseEntityModuleOptions {
                 klass: {},
             }
         },
+        exporter: {
+            processReqForExport(req: CrudRequest, innerFunc) {
+                req.options.query.join = {
+                    student: { eager: true },
+                    teacher: { eager: true },
+                    klass: { eager: true },
+                    lesson: { eager: true }
+                };
+                return innerFunc(req);
+            },
+            getExportHeaders(): IHeader[] {
+                return [
+                    { value: 'teacher.name', label: 'שם המורה' },
+                    { value: 'student.name', label: 'שם התלמידה' },
+                    { value: 'klass.name', label: 'כיתה' },
+                    { value: 'lesson.name', label: 'שיעור' },
+                    { value: 'lessonsCount', label: 'מספר שיעורים' },
+                    { value: getPercentsFormatter('absPercents'), label: 'אחוז חיסור' },
+                    { value: getPercentsFormatter('attPercents'), label: 'אחוז נוכחות' },
+                    { value: getPercentsFormatter('gradeAvg'), label: 'ציון ממוצע' },
+                ];
+            }
+        }
     }
 }
 
