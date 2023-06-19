73080dc577565c3ac87ad541f73c95100756547f
buzz-code <hadasa.schechter@gmail.com>
2023-06-19 10:41:42 +0000

    add migration to set year to current year

---
+++ server/src/migrations/1687171083496-fillCurrentYearOnTables.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class fillCurrentYearOnTables1687171083496 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            update att_reports
+            set year = 5783
+            where year is null or year = 0
+        `);
+        await queryRunner.query(`
+            update grades
+            set year = 5783
+            where year is null or year = 0
+        `);
+        await queryRunner.query(`
+            update lessons
+            set year = 5783
+            where year is null or year = 0
+        `);
+        await queryRunner.query(`
+            update student_klasses
+            set year = 5783
+            where year is null or year = 0
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- todo.md
+++ todo.md
@@ -188,9 +188,9 @@ https://github.com/tinovyatkin/nest-puppeteer
 * add a default year filter
 * check how the teacher excel should look like
 * add default year value on server side for att-report, grade, lesson, student-klass
+* add migration to fill year value if year is null or 0
 
 # todo
-* add migration to fill year value if year is null or 0
 * add impersonate button in user edit form
 * add student number validation - how to do this with year ? maybe student will need to have a year column too
 * fix student attendance list year at backend
