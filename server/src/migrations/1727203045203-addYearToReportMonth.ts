86085d2f3e1d6f15c96a4679f86d041a79144392
buzz-code <hadasa.schechter@gmail.com>
2024-09-24 19:43:15 +0000

    wrap migration in try catch

--- server/src/migrations/1727203045203-addYearToReportMonth.ts
+++ server/src/migrations/1727203045203-addYearToReportMonth.ts
@@ -4,17 +4,21 @@ export class addYearToReportMonth1727203045203 implements MigrationInterface {
     name = 'addYearToReportMonth1727203045203'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
-            ALTER TABLE \`report_month\`
-            ADD \`year\` int NULL
-        `);
-        await queryRunner.query(`
-            CREATE UNIQUE INDEX \`report_month_user_id_start_date_end_date_year_idx\` ON \`report_month\` (\`userId\`, \`startDate\`, \`endDate\`, \`year\`)
-        `);
-        await queryRunner.query(`
-            CREATE UNIQUE INDEX \`report_month_user_id_year_idx\` ON \`report_month\` (\`userId\`, \`year\`)
-        `);
-        
+        try {
+            await queryRunner.query(`
+                ALTER TABLE \`report_month\`
+                ADD \`year\` int NULL
+            `);
+            await queryRunner.query(`
+                CREATE UNIQUE INDEX \`report_month_user_id_start_date_end_date_year_idx\` ON \`report_month\` (\`userId\`, \`startDate\`, \`endDate\`, \`year\`)
+            `);
+            await queryRunner.query(`
+                CREATE UNIQUE INDEX \`report_month_user_id_year_idx\` ON \`report_month\` (\`userId\`, \`year\`)
+            `);
+        } catch (e) {
+            console.log('error while running migration', e)
+        }
+
         await queryRunner.query(`
             UPDATE \`report_month\`
             SET \`year\` = 5782
