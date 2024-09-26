a3c7373593521bdcf9c17e24f21ff6a984ed8047
buzz-code <hadasa.schechter@gmail.com>
2024-09-26 08:30:50 +0000

    add the ability to bulk revert action for audit-log

--- server/src/entity-modules/audit-log.config.ts
+++ server/src/entity-modules/audit-log.config.ts
@@ -1,7 +1,12 @@
-import { BaseEntityModuleOptions } from "@shared/base-entity/interface";
+import { BaseEntityModuleOptions, Entity } from "@shared/base-entity/interface";
 import { AuditLog } from "@shared/entities/AuditLog.entity";
 import { IHeader } from "@shared/utils/exporter/types";
 import { getJsonFormatter } from "@shared/utils/formatting/formatter.util";
+import { CrudRequest } from "@dataui/crud";
+import { BaseEntityService } from "@shared/base-entity/base-entity.service";
+import { In, Repository } from "typeorm";
+import { Student } from "src/db/entities/Student.entity";
+import { Teacher } from "src/db/entities/Teacher.entity";
 
 function getConfig(): BaseEntityModuleOptions {
     return {
@@ -18,7 +23,55 @@ function getConfig(): BaseEntityModuleOptions {
                 ];
             }
         },
+        service: AuditLogService,
     }
 }
+
+const ENTITY_NAME_MAP = {
+    'student': Student,
+    'teacher': Teacher,
+}
+
+class AuditLogService<T extends Entity | AuditLog> extends BaseEntityService<T> {
+    async doAction(req: CrudRequest<any, any>, body: any): Promise<any> {
+        switch (req.parsed.extra.action) {
+            case 'revert': {
+                let successCount = 0;
+                const ids = req.parsed.extra.ids.toString().split(',');
+                const repo = this.repo as Repository<AuditLog>;
+                const auditLogs = await repo.findBy({ id: In(ids) });
+
+                for (const auditLog of auditLogs) {
+                    try {
+                        if (!auditLog.isReverted && auditLog.entityName in ENTITY_NAME_MAP) {
+                            const entityRepo = this.dataSource.getRepository(ENTITY_NAME_MAP[auditLog.entityName]);
+
+                            switch (auditLog.operation) {
+                                case 'DELETE':
+                                    await entityRepo.insert({ ...auditLog.entityData, id: auditLog.entityId });
+                                    break;
+                                default:
+                                    throw new Error(`unknown operation: ${auditLog.operation}`);
+                            }
+
+                            await repo.update({ id: auditLog.id }, { isReverted: true });
+                        }
+                        successCount++;
+                    } catch (e) {
+                        console.log('AuditLogService.revert: error', e);
+                    }
+                }
+
+                if (successCount === ids.length) {
+                    return `reverted ${successCount} items`;
+                } else {
+                    return `reverted ${successCount} items, failed ${ids.length - successCount} items`;
+                }
+            }
+        }
+        return super.doAction(req, body);
+    }
+}
+
 
 export default getConfig();
