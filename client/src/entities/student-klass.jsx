a3a5c4a8e433e3337986d5a15633c680129d29b9
buzz-code <hadasa.schechter@gmail.com>
2024-11-24 16:32:19 +0000

    update sorting logic for student reference fields in student-klass and student-klasses-report components

--- client/src/entities/student-klass.jsx
+++ client/src/entities/student-klass.jsx
@@ -36,7 +36,7 @@ const Datagrid = ({ isAdmin, children, ...props }) => {
             {isAdmin && <TextField source="id" />}
             {isAdmin && <ReferenceField source="userId" reference="user" />}
             <MultiReferenceField source="studentReferenceId" sortBy="student.name" optionalSource="studentTz" reference="student" optionalTarget="tz" />
-            <MultiReferenceField label="תז תלמידה" source="studentReferenceId" sortBy="student.name" optionalSource="studentTz" reference="student" optionalTarget="tz">
+            <MultiReferenceField label="תז תלמידה" source="studentReferenceId" sortBy="student.tz" optionalSource="studentTz" reference="student" optionalTarget="tz">
                 <TextField source="tz" />
             </MultiReferenceField>
             <MultiReferenceField source="klassReferenceId" sortBy="klass.name" optionalSource="klassId" reference="klass" optionalTarget="key" />
