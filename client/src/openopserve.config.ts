0c7f3ea5c265ed2b10acbfe73d62515c2b6e2eb9
buzz-code <hadasa.schechter@gmail.com>
2025-05-14 18:20:48 +0000

    fix: update service name in Openobserve configuration to 'demo-frontend'

--- client/src/openopserve.config.ts
+++ client/src/openopserve.config.ts
@@ -5,7 +5,7 @@ const options = {
     clientToken: '32363366626236332d333131632d343665642d616433372d316332366563386566313330',
     applicationId: 'demo-yoman-online',
     site: 'api.openobserve.ai',
-    service: 'frontend',
+    service: 'demo-frontend',
     env: location.href.includes('dev') ? 'development' : 'production',
     version: packageJson.version,
     organizationIdentifier: 'hadasa_organization_50794_yN95E6myGED9AWO',
