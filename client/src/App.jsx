00b788a4595bc121687f72453b94bb52f521f2a0
buzz-code <hadasa.schechter@gmail.com>
2025-11-09 20:06:43 +0000

    refactor: replace local import of importFile with shared component and remove unused file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -50,7 +50,7 @@ import text from "@shared/components/common-entities/text";
 import textByUser from "@shared/components/common-entities/text-by-user";
 import user from "@shared/components/common-entities/user";
 import auditLog from '@shared/components/common-entities/audit-log';
-import importFile from 'src/entities/import-file';
+import importFile from '@shared/components/common-entities/import-file';
 import mailAddress from '@shared/components/common-entities/mail-address';
 import recievedMail from '@shared/components/common-entities/recieved-mail';
 import page from '@shared/components/common-entities/page';
