d2ec9021e119a90e5a39b01f1b32d4203ea4148c
buzz-code <hadasa.schechter@gmail.com>
2025-12-10 17:05:33 +0000

    feat: enhance known absence configuration with export headers and eager loading

--- server/src/entity-modules/known-absence.config.ts
+++ server/src/entity-modules/known-absence.config.ts
@@ -1,4 +1,6 @@
+import { CrudRequest } from "@dataui/crud";
 import { BaseEntityModuleOptions } from "@shared/base-entity/interface";
+import { IHeader } from "@shared/utils/exporter/types";
 import { KnownAbsence } from "src/db/entities/KnownAbsence.entity";
 
 function getConfig(): BaseEntityModuleOptions {
@@ -7,8 +9,32 @@ function getConfig(): BaseEntityModuleOptions {
         query: {
             join: {
                 student: { eager: false },
+                lesson: { eager: false },
+                klass: { eager: false },
             }
         },
+        exporter: {
+            processReqForExport(req: CrudRequest, innerFunc) {
+                req.options.query.join = {
+                    student: { eager: true },
+                };
+                return innerFunc(req);
+            },
+            getExportHeaders(): IHeader[] {
+                return [
+                    { value: 'student.name', label: 'תלמידה' },
+                    { value: 'klass.name', label: 'כיתה' },
+                    { value: 'lesson.name', label: 'שיעור' },
+                    { value: 'reportDate', label: 'תאריך דיווח' },
+                    { value: 'absnceCount', label: 'מספר היעדרויות' },
+                    { value: 'absnceCode', label: 'קוד היעדרות' },
+                    { value: 'senderName', label: 'שם השולח' },
+                    { value: 'reason', label: 'סיבה' },
+                    { value: 'comment', label: 'הערה' },
+                    { value: 'isApproved', label: 'מאושר' },
+                ];
+            }
+        }
     }
 }
 
