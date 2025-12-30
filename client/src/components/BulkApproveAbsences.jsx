658aeb7dbe31051594b40a94b21642a6031a719c
yaely20 <145603125+yaely20@users.noreply.github.com>
2025-12-30 21:20:32 +0000

    Change permissions

--- client/src/components/BulkApproveAbsences.jsx
+++ client/src/components/BulkApproveAbsences.jsx
@@ -9,7 +9,7 @@ export const BulkApproveAbsences = ({ label, name, permissions }) => {
     const resource = useResourceContext();
     const refresh = useRefresh();
     const notify = useNotify();
-    if (isApprovedAbsencesBulk(permissions)) return null;
+    if ( !isApprovedAbsencesBulk(permissions)) return null;
     
      const handleSave = async (values, onClose) => {
         const payload = values.approvedAbsences.map(row => ({
