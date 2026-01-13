fccd2bb8b309ca4724334d0ab2cbbda2843a7c1a
yael-miller <mironyael49@gmail.com>
2026-01-13 20:36:28 +0000

    addTransportationTable

---
+++ server/src/migrations/1768335982284-addTransportationTable.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class addTransportationTable1768335982284 implements MigrationInterface {
+    name = 'addTransportationTable1768335982284'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE \`transportations\` (
+                \`id\` int NOT NULL AUTO_INCREMENT,
+                \`user_id\` int NOT NULL,
+                \`year\` int NULL,
+                \`key\` int NOT NULL,
+                \`departure_time\` varchar(255) NULL,
+                \`description\` varchar(1000) NULL,
+                \`created_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+                \`updated_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+                UNIQUE INDEX \`IDX_786464b82be79319c981bfd00a\` (\`user_id\`, \`key\`, \`year\`),
+                INDEX \`transportations_users_idx\` (\`user_id\`),
+                PRIMARY KEY (\`id\`)
+            ) ENGINE = InnoDB
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP INDEX \`transportations_users_idx\` ON \`transportations\`
+        `);
+        await queryRunner.query(`
+            DROP INDEX \`IDX_786464b82be79319c981bfd00a\` ON \`transportations\`
+        `);
+        await queryRunner.query(`
+            DROP TABLE \`transportations\`
+        `);
+    }
+
+}
