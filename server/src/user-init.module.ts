1996811a2ec6df0a7914cadf79b3c3bf1ac746e9
Hadasa Schechter <72219643+buzz-code@users.noreply.github.com>
2026-05-03 10:39:53 +0000

    feat: migrate to BaseNraAppModule.forRoot and createSharedEntitiesImports (Day 6)

--- server/src/user-init.module.ts
+++ server/src/user-init.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { USER_INITIALIZATION_SERVICE } from '@shared/auth/user-initialization.interface';
 import { UserInitializationService } from './user-initialization.service';
 import { ReportMonth } from './db/entities/ReportMonth.entity';
 
@@ -7,7 +8,10 @@ import { ReportMonth } from './db/entities/ReportMonth.entity';
   imports: [
     TypeOrmModule.forFeature([ReportMonth]),
   ],
-  providers: [UserInitializationService],
-  exports: [UserInitializationService],
+  providers: [
+    UserInitializationService,
+    { provide: USER_INITIALIZATION_SERVICE, useExisting: UserInitializationService },
+  ],
+  exports: [UserInitializationService, USER_INITIALIZATION_SERVICE],
 })
 export class UserInitModule { }
