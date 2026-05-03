8f9a8a700ad2c039e4ce96c6f72ed15f8f8f7848
Hadasa Schechter <72219643+buzz-code@users.noreply.github.com>
2026-05-03 10:13:42 +0000

    feat: use shared AppController, AppService, and bootstrapNraApplication

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,10 +1,1 @@
-import { Injectable } from '@nestjs/common';
-
-const serverStartTime = new Date();
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return `Hello World! Server started at ${serverStartTime.toISOString()}`;
-  }
-}
+export { AppService } from '@shared/app/app.service';
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,19 +1,5 @@
 import '@shared/config/crud.config';
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'src/app.module';
-import { setupApplication, setupYemotRouter } from '@shared/utils/bootstrap.util';
+import { bootstrapNraApplication } from '@shared/utils/bootstrap.util';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  setupApplication(app, {
-    swaggerTitle: 'react-admin-nestjs',
-    swaggerDescription: 'Demo website description',
-    swaggerTag: 'demo',
-  });
-
-  setupYemotRouter(app);
-
-  await app.listen(3000);
-}
-bootstrap();
+bootstrapNraApplication(AppModule);
